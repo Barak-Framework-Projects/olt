7db81babdfb8aacc25862334aa072d2b6139191e
Gökhan Demir <mail@gdemir.me>
2016-12-23 15:31:01 +0200

    Update show.php

--- app/views/home/producttypepage/show.php
+++ app/views/home/producttypepage/show.php
@@ -7,7 +7,7 @@
 </ol>
 
 <!-- first section - Home -->
-<div class="parallax" style="background-image: url(<?= $producttype->image; ?>)">
+<div class="parallax" style="background-image: url(<?= $producttype->image; ?>);">
   <div class="parallax-caption">
     <h1 class="background-transparent"><?= $producttype->name; ?></h1>
   </div>
